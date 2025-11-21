448b97dd4a40b449260292ffaab70db5de33da53
uxclub-vitb <ux_club@vitbhopal.ac.in>
2025-11-22 01:22:46 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -45,11 +45,11 @@ const Header = () => {
   return (
     <>
       <div
-        className={`fixed top-0 left-0 w-full bg-[#dcfebc] lg:bg-white z-50 transition-transform duration-300 ${
+        className={`fixed top-0 left-0 w-full bg-white hover:bg-[#dcfebc] z-50 transition-all duration-300 ${
           showHeader ? "translate-y-0" : "-translate-y-full"
         }`}
       >
-        <div className="w-full h-18 lg:h-20 border-b flex justify-between items-center ">
+        <div className="w-full h-18 lg:h-20 border-b flex justify-between items-center">
           <Link to="/">
             <svg
               viewBox="50 -10 500 100"
